857e42f50e00e15f060717b6283f32660a3dd93a
lakshmi <soo280902@gmail.com>
2023-08-22 18:56:30 +0530

    app

--- main.py
+++ main.py
@@ -22,3 +22,63 @@ fig1 = px.bar(job_count,job_count.index,job_count.values,title='Job titles of th
 st.plotly_chart(fig1, use_container_width=True)
 st.subheader('these are the popular jobs')
 st.caption(",".join(job_count.index.tolist()))
+
+st.subheader("these are popular jobs")
+st.info(",".join(job_count.index.tolist()))
+
+#main questions from dataset 
+st.markdown('''
+## What can we find out ?)
+- salary trend on the basis of   
+    -year    
+     -experience   
+    -employement type   
+    -job title   
+    -location   
+    -company size   
+    -currency   
+- statistical analysis of diffent category vs salary  
+''')   
+
+
+categories = df.select_dtypes(exclude=np.number).columns.tolist()
+st.success(f'There are following categories: {",".join(categories)}')
+for col in categories:
+    counts = df[col].value_counts()
+    if df[col].nunique()>10:
+        fig=px.bar(counts,counts.index,counts.values,log_y=True,text=counts.values,title=f'Distribution of {col}')
+        fig.update_traces(texttemplate='%{text:.25}',textposition='outside')
+    else:
+        fig = px.pie(counts,counts.index,counts.values,title=f'Distribution of {col}')
+    st.plotly_chart(fig,use_container_width=True)
+
+
+
+year_wise_df_sum = df.groupby('work_year')[['salary','salary_in_usd']].sum().reset_index()
+year_wise_df_mean = df.groupby('work_year')[['salary','salary_in_usd']].mean().reset_index()
+st.dataframe(year_wise_df_sum)
+
+fig = px.bar(year_wise_df_sum,'work_year','salary_in_usd',title=' Salary trend over the years')
+fig2 = px.bar(year_wise_df_mean,'work_year','salary_in_usd',title='Mean Salary trend over the years')
+c1,c2=st.columns(2)
+c1.plotly_chart(fig,use_container_width=True)
+c2.plotly_chart(fig,use_container_width=True)
+
+
+cats=c1.multiselect('select categories',categories)
+graphs = ['box','violin','swarp','sunburst','treemap']
+graph = c2.selectbox('Select graph',graphs)
+
+for col in cats:
+    if graph == graphs[0]:
+        fig = px.box(df,x=col,y='salary_in_usd',title=f'Salary distribution of {col}')
+    elif graph == graphs[1]:
+        fig = px.violin(df,x=col,y='salary_in_usd',title=f'Salary distribution of {col}') 
+    elif graph == graphs[2]:
+        fig = px.bar_polar(df,r='salary_in_usd',theta=col,title=f'Salary distribution of {col}')
+    st.plotly_chart(fig,use_container_width=True)
+if graph == graphs[3]:
+    fig = px.sunburst(df,path=cats,values='salary_in_usd',title=f'Salary distribution of {col}')   
+if graph == graphs[4]:
+    fig = px.treemap(df,path=cats,values='salary_in_usd',title=f'Salary distribution of {col}')
+st.plotly_chart(fig,use_container_width=True)    
